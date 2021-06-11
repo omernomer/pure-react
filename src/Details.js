d16abd20e268b953cbc2bf56e4bd77ecf27232c7
Omar Al-Debagy <omeraldebagy@gmail.com>
2021-06-11 17:25:49 +0200

    11/6

--- src/Details.js
+++ src/Details.js
@@ -1,14 +1,44 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
+import Slider from "./Slider";
 
 class Details extends Component {
-  constructor() {
-    super();
+  state = {
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
   }
-  state = {};
   render() {
-    return <h2>Hi</h2>;
+    if (this.state.loading) {
+      return <h2>Loading...</h2>;
+    }
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
+    return (
+      <div className="details">
+        <Slider images={images} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
+          <p>{description}</p>
+          <button>Adopt {name}</button>
+        </div>
+      </div>
+    );
   }
 }
 
-export default Details;
+export default withRouter(Details);
---
+++ src/Slider.js
@@ -0,0 +1,38 @@
+import { Component } from "react";
+
+class Slider extends Component {
+  state = { active: 0 };
+
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+
+  handleIndexClick = (event) => {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
+  };
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            <img
+              key={photo}
+              src={photo}
+              data-index={index}
+              onClick={this.handleIndexClick}
+              className={index === active ? "active" : ""}
+              alt="animal-thumb"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Slider;
